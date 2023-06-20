92e2b211ac995848cad4ac165a9a0f93a075e6aa
Mohit Kamdi <130160983+Mohit27071998@users.noreply.github.com>
2023-06-20 15:58:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+<sections id="buttons">
+      <button class="btn">applause</button>
+      <button class="btn">boo</button>
+      <button class="btn">gasp</button>
+      <button class="btn">tada</button>
+      <button class="btn">victory</button>
+      <button class="btn">wrong</button>
+      <button class="btn stop">stop</button>
+    </sections>
